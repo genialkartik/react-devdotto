00ef02627b44830f85b9411f1d01b1222aa49331
Abhishek Adhikari <abhiayush23@gmail.com>
2021-05-15 20:54:25 +0530

    Dark Mode Toggle and ClassName Props Added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React ,{Component}from 'react';
-import { render } from "react-dom";
-import DevDotTo from "./lib";
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import DevDotTo from './lib';
+import './index.css';
 
 class App extends Component {
-  render(){
-    return(
-      <DevDotTo username="genialkartik" />
-    )
-  }
+	render() {
+		return <DevDotTo username="genialkartik" darkMode={false} className="test" />;
+	}
 }
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
