364cfba2e74d1de413b24dbb04f38496afe4ea27
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-09-10 09:28:32 -0300

    Refactor + added the parameter numberOfPosts (#11)
    Made some refactoring and added the parameter numberOfPosts which lets you filter how many posts you want to retrieve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React ,{Component}from 'react';
-import { render } from "react-dom";
-import DevDotTo from "./lib";
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import DevDotTo from './lib'
 
 class App extends Component {
-  render(){
-    return(
-      <DevDotTo username="genialkartik" />
-    )
+  render() {
+    return <DevDotTo username='genialkartik' />
   }
 }
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'))
